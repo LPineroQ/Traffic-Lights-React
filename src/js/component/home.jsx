c9ac72fce247dbeaebff8c1491021db55524da21
Luis Piñero Quintana <lpineroq@gmail.com>
2022-01-19 19:49:57 +0000

    Changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,4 @@
 import React from "react";
-import Footer from "./footer.jsx";
-import Cta from "./cta.jsx";
-import NavBar from "./navbar.jsx";
-import Cards from "./Cards.jsx";
-
-<<<<<<< HEAD
-//include images into your bundle
 import TrafficLights from "./trafficLights.jsx";
 
 //create your first component
@@ -14,21 +7,6 @@ const Home = () => {
 		<div>
 			<TrafficLights></TrafficLights>
 		</div>
-=======
-const Home = () => {
-	return (
-		<div>
-    <NavBar></NavBar>
-			<Cta />
-			<div className="container d-flex">
-				<Cards></Cards>
-				<Cards></Cards>
-				<Cards></Cards>
-				<Cards></Cards>
-			</div>
-     <Footer />
-    </div>
->>>>>>> cd32d7fe14dd452532fa2f9d9bc9dcbb95cbd0d8
 	);
 };
 
